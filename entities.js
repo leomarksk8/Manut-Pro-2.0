453377e5d12bffc2ff3ed005cb80f7faaaa3e92b
leomarksk8 <leomarksk8@gmail.com>
2025-12-05 18:19:14 -0300

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,27 @@
+import { base44 } from './base44Client';
+
+
+export const Colaborador = base44.entities.Colaborador;
+
+export const Equipamento = base44.entities.Equipamento;
+
+export const Alocacao = base44.entities.Alocacao;
+
+export const Turno = base44.entities.Turno;
+
+export const AnotacaoTurno = base44.entities.AnotacaoTurno;
+
+export const HistoricoRelatorio = base44.entities.HistoricoRelatorio;
+
+export const RegistroAssiduidade = base44.entities.RegistroAssiduidade;
+
+export const LiberacaoEquipamento = base44.entities.LiberacaoEquipamento;
+
+export const ProgramacaoSemanal = base44.entities.ProgramacaoSemanal;
+
+export const TratativaOM = base44.entities.TratativaOM;
+
+
+
+// auth sdk:
+export const User = base44.auth;
